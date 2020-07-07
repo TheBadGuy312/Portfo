0be103b19077c1e255aede255607427fc1ad10d6
unknown <mmssatish007@gmail.com>
2020-07-07 16:52:12 +0530

    my portfolio website

---
+++ server.py
@@ -0,0 +1,47 @@
+from flask import Flask,render_template,request,redirect
+import csv
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def about(page_name):
+    return render_template(page_name)
+
+def write_to_file(data):
+	with open('database.txt',mode='a')as database:
+		email=data["email"]
+		subject=["subject"]
+		message=data["message"]
+		file= database.write(f'\n {email},{subject},{message}')
+
+def write_to_csv(data):
+	with open('database.csv',newline='',mode='a')as database2:
+		email=data["email"]
+		subject=data["subject"]
+		message=data["message"]
+		csv_writer=csv.writer(database2,delimiter=',', quotechar='"',quoting =csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
+
+
+@app.route('/submit_form',methods=['POST','GET'])
+def submit_form():
+	if request.method=='POST':
+		data = request.form.to_dict()
+		write_to_csv(data)
+		return redirect('/thankyou.html')
+	else:
+		return "Something's wrong"
+
+
+# @app.route('/<username>')
+# def hello_world(username=None):
+#     return render_template('index.html',name=username)
+
+
+
+# set FLASK_ENV=development
+# set FLASK_APP=server.py
+# flask run    
